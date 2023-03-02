f9b9c10dfe0f739938ec08c9d8a707ffe18f26ad
Joseph-Lumpkin <43595505+Joseph-Lumpkin@users.noreply.github.com>
2023-03-02 18:32:27 -0500

    Created the LoginActivity
    Created the basic template for the LoginActivity.
    This uses google's One Tap Client authorization to authenticate users and avoid us needing to handle PII in the login activity.

---
+++ Squibb-Android/app/src/main/java/com/squibb/android/LoginActivity.kt
@@ -0,0 +1,148 @@
+package com.squibb.android
+
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.identity.SignInCredential
+import com.google.android.gms.common.api.ApiException
+import com.squibb.android.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    /** Debug logging TAG */
+    private val TAG = "LoginActivity"
+
+    /** View Binding */
+    private lateinit var mBinding: ActivityLoginBinding
+
+    /** Google One Tap Client authorization variables */
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+    private lateinit var signUpRequest: BeginSignInRequest
+    private lateinit var credential: SignInCredential
+    private val REQ_ONE_TAP = 2
+
+    /** Statically defined keywords */
+    companion object {
+        const val dsKEY_ID_TOKEN = "idToken"
+        //TODO Remove reference to the user's email credentials. We don't want to deal with PII
+        const val dsKEY_EMAIL = "email"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialize the view binding for nonnullable view referencing
+        mBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        // Initialize the oneTapClient for our activity
+        oneTapClient = Identity.getSignInClient(this)
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(
+                BeginSignInRequest.PasswordRequestOptions.builder()
+                    .setSupported(true)
+                    .build()
+            )
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(BuildConfig.API_KEY)
+                    .setFilterByAuthorizedAccounts(false)
+                    .build()
+            )
+            // Do not automatically sign in when exactly one credential is retrieved.
+            .setAutoSelectEnabled(false)
+            .build()
+        // Google Sign Up Request
+        signUpRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(BuildConfig.API_KEY)
+                    .setFilterByAuthorizedAccounts(true)
+                    .build()
+            )
+            .build()
+        // Wait for the user initiate the login process with google
+        mBinding.bLogin.setOnClickListener { signIn() }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+                    credential = oneTapClient.getSignInCredentialFromIntent(data)
+                    val idToken = credential.googleIdToken
+                    val email = credential.id
+                    // Got an ID token from Google. Use it to authenticate with your backend.
+                    Log.d(TAG, "Got ID token: $idToken")
+                    // Got a saved username
+                    //TODO Remove reference to the user's email credentials. We don't want to deal with PII
+                    Log.d(TAG, "Got username: $email")
+                    if (email != null) {
+                        val finishLoginIntent = Intent()
+                        finishLoginIntent.putExtra(dsKEY_ID_TOKEN, idToken)
+                        finishLoginIntent.putExtra(dsKEY_EMAIL, email)
+                        setResult(Activity.RESULT_OK, finishLoginIntent)
+                        finish()
+                    }
+                } catch (e: ApiException) {
+                    println("error")
+                }
+            }
+        }
+    }
+
+    /**
+     * Display the OneTapClient Sign In UI
+     */
+    private fun signIn() {
+        oneTapClient.beginSignIn(signInRequest)
+            .addOnSuccessListener(this) { result ->
+                try {
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender, REQ_ONE_TAP,
+                        null, 0, 0, 0, null
+                    )
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            .addOnFailureListener(this) { e ->
+                // No saved credentials found. Launch the One Tap sign-up flow.
+                signUp()
+            }
+    }
+
+
+    /**
+     * Display the OneTapClient Sign Up UI
+     */
+    private fun signUp() {
+        //TODO Test the signup flow
+        oneTapClient.beginSignIn(signUpRequest)
+            .addOnSuccessListener(this) { result ->
+                try {
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender, REQ_ONE_TAP,
+                        null, 0, 0, 0, null
+                    )
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            .addOnFailureListener(this) { e ->
+                // User failed to sign up
+                // Do nothing and continue presenting the signed-out UI.
+                Log.d(TAG, e.localizedMessage)
+            }
+    }
+}
--- Squibb-Android/app/src/main/java/com/squibb/android/MainActivity.kt
+++ Squibb-Android/app/src/main/java/com/squibb/android/MainActivity.kt
@@ -1,51 +1,53 @@
 package com.squibb.android
 
+import android.app.Activity
 import android.content.Intent
-import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.android.gms.auth.api.identity.SignInCredential
-import com.google.android.gms.common.api.ApiException
 import com.google.firebase.FirebaseApp
 import com.squibb.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    // Debug logging TAG
+    /** Debug logging TAG */
     private val TAG = "MainActivity"
 
-    private lateinit var binding: ActivityMainBinding
+    /** View Binding */
+    private lateinit var mBinding: ActivityMainBinding
 
-    private lateinit var oneTapClient: SignInClient
-    private lateinit var signInRequest: BeginSignInRequest
-    private lateinit var signUpRequest: BeginSignInRequest
-    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
-    private var showOneTapUI = true
-    private lateinit var credential: SignInCredential
+    /** LoginActivity Result Listener */
+    private val mLoginActivityLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            // If the login result was OK (logged in successfully)
+            if (result.resultCode == Activity.RESULT_OK) {
+                // Retrieve the credentials and save the retrieved user account
+                val data = result.data
+                val idToken = data?.getStringExtra(LoginActivity.dsKEY_ID_TOKEN)
+                Log.d(TAG, "$idToken successfully retrieved and returned to $TAG.")
+                //TODO Remove reference to the user's email credentials. We don't want to deal with PII
+                val email = data?.getStringExtra(LoginActivity.dsKEY_EMAIL)
+                Log.d(TAG, "$email successfully logged in and returned to $TAG.")
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Initialize the firebase app
         FirebaseApp.initializeApp(applicationContext)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
+        // Initialize the view binding
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        // Initialize the nav view and controller
+        val navView: BottomNavigationView = mBinding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
@@ -54,106 +56,25 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        oneTapClient = Identity.getSignInClient(this)
-        signInRequest = BeginSignInRequest.builder()
-            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
-                .setSupported(true)
-                .build())
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId(BuildConfig.API_KEY)
-                    // Only show accounts previously used to sign in.
-                    .setFilterByAuthorizedAccounts(false)
-                    .build())
-            // Automatically sign in when exactly one credential is retrieved.
-            .setAutoSelectEnabled(true)
-            .build()
-
-        signUpRequest = BeginSignInRequest.builder()
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId(BuildConfig.API_KEY)
-                    // Show all accounts on the device.
-                    .setFilterByAuthorizedAccounts(false)
-                    .build())
-            .build()
-
-        findViewById<Button>(R.id.bDebug).setOnClickListener { debug() }
-        findViewById<Button>(R.id.bSignin).setOnClickListener { signIn() }
+        // Always require the user to sign in
+        goToLoginActivity()
     }
 
-    private fun signIn() {
-        oneTapClient.beginSignIn(signInRequest)
-            .addOnSuccessListener(this) { result ->
-                try {
-                    startIntentSenderForResult(
-                        result.pendingIntent.intentSender, REQ_ONE_TAP,
-                        null, 0, 0, 0, null)
-                } catch (e: IntentSender.SendIntentException) {
-                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
-                }
-            }
-            .addOnFailureListener(this) { e ->
-                // No saved credentials found. Launch the One Tap sign-up flow, or
-                // do nothing and continue presenting the signed-out UI.
-
-                oneTapClient.beginSignIn(signUpRequest)
-                    .addOnSuccessListener(this) { result ->
-                        try {
-                            startIntentSenderForResult(
-                                result.pendingIntent.intentSender, REQ_ONE_TAP,
-                                null, 0, 0, 0)
-                        } catch (e: IntentSender.SendIntentException) {
-                            Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
-                        }
-                    }
-                    .addOnFailureListener(this) { e ->
-                        // No Google Accounts found. Just continue presenting the signed-out UI.
-                        Log.d(TAG, e.localizedMessage)
-                    }
-
-                Log.d(TAG, e.localizedMessage)
-            }
+    /**
+     * Launch the LoginActivity
+     * and prompt the user to log in.
+     */
+    private fun goToLoginActivity() {
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        mLoginActivityLauncher.launch(loginIntent)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        when (requestCode) {
-            REQ_ONE_TAP -> {
-                try {
-                    credential = oneTapClient.getSignInCredentialFromIntent(data)
-                    val idToken = credential.googleIdToken
-                    val username = credential.id
-                    val password = credential.password
-                    when {
-                        idToken != null -> {
-                            // Got an ID token from Google. Use it to authenticate
-                            // with your backend.
-                            Log.d(TAG, "Got ID token.")
-                        }
-                        password != null -> {
-                            // Got a saved username and password. Use them to authenticate
-                            // with your backend.
-                            Log.d(TAG, "Got password.")
-                        }
-                        else -> {
-                            // Shouldn't happen.
-                            Log.d(TAG, "No ID token or password!")
-                        }
-                    }
-                } catch (e: ApiException) {
-                    println("error")
-                }
-            }
-        }
-    }
-
+    /**
+     * Debug function.
+     * Definition and function will change throughout development.
+     * Use this function to debug the application.
+     */
     private fun debug() {
-        println(credential)
+        println("")
     }
 }
