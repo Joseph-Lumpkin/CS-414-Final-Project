090498f7cf07259f16f8b3d1279e3c4f2e196273
Joseph-Lumpkin <43595505+Joseph-Lumpkin@users.noreply.github.com>
2023-03-03 11:47:32 -0500

    Added Additional Activities
    Added the additional activities that will be needed to achieve desired app functionality.
    Added the navigation skeleton to reach the Join Group Activity screen.

---
+++ Squibb-Android/app/src/main/java/com/squibb/android/GroupJoinActivity.kt
@@ -0,0 +1,11 @@
+package com.squibb.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GroupJoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_join)
+    }
+}
--- Squibb-Android/app/src/main/java/com/squibb/android/ui/groups/GroupsFragment.kt
+++ Squibb-Android/app/src/main/java/com/squibb/android/ui/groups/GroupsFragment.kt
@@ -1,5 +1,6 @@
 package com.squibb.android.ui.groups
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,36 +8,49 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.squibb.android.GroupJoinActivity
 import com.squibb.android.databinding.FragmentGroupsBinding
 
 class GroupsFragment : Fragment() {
 
-    private var _binding: FragmentGroupsBinding? = null
+    private var mBinding: FragmentGroupsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+    private val binding get() = mBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Setup the view binding
         val groupsViewModel =
             ViewModelProvider(this).get(GroupsViewModel::class.java)
-
-        _binding = FragmentGroupsBinding.inflate(inflater, container, false)
+        mBinding = FragmentGroupsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGroups
         groupsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        // Initialize the view properties
+        initViews()
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        mBinding = null
+    }
+
+    private fun initViews() {
+        // Set onClickListeners
+        mBinding?.bJoin?.setOnClickListener { launchJoinGroupActivity() }
+    }
+
+    private fun launchJoinGroupActivity() {
+        val launchJoinGroupIntent = Intent(context, GroupJoinActivity::class.java)
+        startActivity(launchJoinGroupIntent)
     }
 }
